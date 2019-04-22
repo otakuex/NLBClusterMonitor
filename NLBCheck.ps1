58aa15875e897ac76e12e49ba46131338322f2ec
otakuex <42971364+otakuex@users.noreply.github.com>
2019-04-22 11:33:40 -0400

    Cleaning script

--- NLBCheck.ps1
+++ NLBCheck.ps1
@@ -40,26 +40,26 @@ $Outputreport = '<HTML>
     </TR>
 '
 
-$Outputreport += nlbsrvchk -srvname HWCFTLACTRL001 -clusternum 3 -Description "Talks to ALL AServers that handle and deliver the content."
-$Outputreport += nlbsrvchk -srvname HWCFTLAMGMT001 -clusternum 2 -description "Manages / creates ALL Backend services (Channels)"
-$Outputreport += nlbsrvchk -srvname HWCFTLBMGMT001 -clusternum 2 -description "Manages ALL branch level machines (Ch MAPs, VOD content, Accts, etc…)"
-$Outputreport += nlbsrvchk -srvname HWCFTLCFSG101 -clusternum 5 -description "Collects data from STBs"
-$Outputreport += nlbsrvchk -srvname HWCFTLCGSG101 -clusternum 5 -description "Delivers client SW and get the STB’s connected to the platform"
-$Outputreport += nlbsrvchk -srvname HWCFTLHPNR001 -clusternum 2 -description "Delivers High Priority messages to the STB’s."
-$Outputreport += nlbsrvchk -srvname HWCFTLMDSFE101 -clusternum 5 -description "Manage/maintain the communication between STB and Storefront"
-$Outputreport += nlbsrvchk -srvname HWCFTLMDSING001 -clusternum 2 -description "Ingest EPG, Asset metadata information."
-$Outputreport += nlbsrvchk -srvname HWCFTLMFAPP001 -clusternum 5 -description "Storefront"
-$Outputreport += nlbsrvchk -srvname HWCFTLMMGMT001 -clusternum 2 -description "Contracts for Assets on our storefront"
-$Outputreport += nlbsrvchk -srvname HWCFTLPOSTER001 -clusternum 2 -description "Maintains posters updated in our lineup over GUIDE"
-$Outputreport += nlbsrvchk -srvname HWCFTLSFBR001 -clusternum 2 -description "Ingest EPG, propagate the Keys from Backend to Branch."
-$Outputreport += nlbsrvchk -srvname HWCFTLSFSG101 -clusternum 2 -description "Maintain DVR’s in sync to the platform."
-$Outputreport += nlbsrvchk -srvname HWCFTLSYNC001 -clusternum 2 -description "Does the first contact with the STB and provide the initial SW file."
+$Outputreport += nlbsrvchk -srvname FTLACTRL001 -clusternum 3 -Description "Group A controller"
+$Outputreport += nlbsrvchk -srvname FTLAMGMT001 -clusternum 2 -description "Group A management"
+$Outputreport += nlbsrvchk -srvname FTLBMGMT001 -clusternum 2 -description "Group B management"
+$Outputreport += nlbsrvchk -srvname FTLCFSG101 -clusternum 5 -description "Client Facing Service Gateway"
+$Outputreport += nlbsrvchk -srvname FTLCGSG101 -clusternum 5 -description "Client Group Service Gateway"
+$Outputreport += nlbsrvchk -srvname FTLHPNR001 -clusternum 2 -description "Delivers High Priority messages to clients."
+$Outputreport += nlbsrvchk -srvname FTLMDSFE101 -clusternum 5 -description "Manage/maintain the communication between clients and database"
+$Outputreport += nlbsrvchk -srvname FTLMDSING001 -clusternum 2 -description "MDS ingestion."
+$Outputreport += nlbsrvchk -srvname FTLMFAPP001 -clusternum 5 -description "Feature application"
+$Outputreport += nlbsrvchk -srvname FTLMMGMT001 -clusternum 2 -description "Media management"
+$Outputreport += nlbsrvchk -srvname FTLPOSTER001 -clusternum 2 -description "Maintains posters images"
+$Outputreport += nlbsrvchk -srvname FTLSFBR001 -clusternum 2 -description "Ingest source data, propagate the Keys from Backend to Branch."
+$Outputreport += nlbsrvchk -srvname FTLSFSG101 -clusternum 2 -description "Maintain clients' in sync to the platform."
+$Outputreport += nlbsrvchk -srvname FTLSYNC001 -clusternum 2 -description "Does the first contact with the clients and provide the initial SW file."
 
 $Outputreport += "</Table>
 </BODY>
 </HTML>"
 
-$Outputreport | Out-File C:\Scripts\NLBCheck.htm
+$Outputreport | Out-File C:\Scripts\NLBCheck.htm # Change output file path as desired, but make sure to make the same changes to the 2 references below
 
 # Email the HTML report with custom subject
 
@@ -69,8 +69,8 @@ if($count -gt 1) {$subject = "Daily NLB Check - $count CLUSTERS DEGRADED"}
 elseif($count -eq 1) {$subject = "Daily NLB Check - 1 CLUSTER DEGRADED"}
 else {$subject = "Daily NLB Check - All OK"}
 
-$smtpServer = "smtp1.HWC.IPTV.COM"
-$smtpFrom = "not-reply-MediaroomAlert@hotwirecommunication.com"
+$smtpServer = "smtp1.YOURDOMAIN.COM" #enter your own SMTP server DNS name / IP address here
+$smtpFrom = "Not-Reply-Alert@someemail.com"
 $smtpTo = "receipentaddress@test.com"
 
 $message = New-Object System.Net.Mail.MailMessage $smtpfrom, $smtpto
@@ -79,9 +79,9 @@ $message.Body = "<head><pre>$style</pre></head>"
 
 $smtp = New-Object Net.Mail.SmtpClient($smtpServer)
 
-$users = "emanuele.jones@hotwiremail.com","jrodriguez@hotwiremail.com","flopez@hotwiremail.com","rob.reidy@hotwiremail.com","amit.eshet@hotwiremail.com","christopher.peraza@hotwirecommunication.com","kelly.webb@hotwirecommunication.com" # List of users to email your report to (separate by comma)
-$fromemail = "not-reply-MediaroomAlert@hotwirecommunication.com"
-$server = "smtp1.HWC.IPTV.COM" #enter your own SMTP server DNS name / IP address here
+$users = "user1@someemail.com","user2@someemail.com","user3@someemail.com","user4@someemail.com" # List of users to email your report to (encapsulate in quotes, separate by comma)
+$fromemail = "Not-Reply-Alert@someemail.com"
+$server = "smtp1.YOURDOMAIN.COM" #enter your own SMTP server DNS name / IP address here
 $a = (Get-Date).Hour
 $message.Body += Get-Content C:\Scripts\NLBCheck.htm
 
